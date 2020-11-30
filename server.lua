58405b6bad616383418702f1813ee6d3a07473bb
中汁龍 <72063585+XNORGATE@users.noreply.github.com>
2020-11-30 20:42:51 +0800

    Update and rename server.lua  to server.lua

--- server.lua
+++ server.lua
@@ -1,7 +1,8 @@
 ESX = nil
 
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+
 RegisterServerEvent('xng_clearveh')
 AddEventHandler('xng_clearveh', function()
-TriggerClientEvent('chatMessage', -1, "[城市通知]", {238, 78, 118}, "無人車輛"..Config.clearafternoti.."秒後即將被拖吊") 
+ TriggerClientEvent('chatMessage', -1, "[城市通知]", {238, 78, 118}, "無人車輛"..Config.clearafternoti.."秒後即將被拖吊") 
 end)
