ff41328e30ea9408864fe71a804dcda07e5c6908
中汁龍 <72063585+XNORGATE@users.noreply.github.com>
2020-11-30 20:00:45 +0800

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,10 +1,10 @@
 ESX = nil
-
+local chatColor = {238, 78, 118}
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
 RegisterCommand("clearveh", function()
 
-TriggerEvent('chatMessage', -1, "[城市通知]", {238, 78, 118}, "無人車輛"..Config.clearafternoti.."秒後即將被拖吊") 
+TriggerEvent('chatMessage', -1, "[城市通知]", chatColor, "無人車輛"..Config.clearafternoti.."秒後即將被拖吊") 
 
 Citizen.Wait(Config.clearafternoti*1000)
 	local cars = ESX.Game.GetVehicles()
@@ -24,7 +24,7 @@ Citizen.CreateThread(function()
 while Config.autoclear do
   Citizen.Wait(Config.autoclearhour*36000000)
 	local cars = ESX.Game.GetVehicles()
-   TriggerEvent('chatMessage', -1, "[城市通知]", {238, 78, 118}, "無人車輛"..Config.clearafternoti.."秒後即將被拖吊") 
+   TriggerEvent('chatMessage', -1, "[城市通知]", chatColor, "無人車輛"..Config.clearafternoti.."秒後即將被拖吊") 
     Citizen.Wait(Config.clearafternoti*1000)
 	for i=1, #cars, 1 do
     local passengers = GetVehicleNumberOfPassengers(cars[i])
