62ebe6f0030d8f5b49ef361cc09d37cd3a05087b
中汁龍 <72063585+XNORGATE@users.noreply.github.com>
2020-12-04 01:06:03 +0800

    Update config.lua

--- config.lua
+++ config.lua
@@ -4,6 +4,6 @@ Config.clearafternoti  = 10   -----後公頻通知後10秒清除伺服器無人�
 
 ---伺服器冷卻時間自動清除無人車輛
 Config.autoclear = true ---是否開啟自動清除
-Config.autoclearhour   = 2 ------2小時自動清除車輛(公頻會通知)
+Config.min   = 10 ------十分鐘自動清除車輛(公頻會通知)
 
 ---清車指令 /clearveh
