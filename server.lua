e88363975d8f3b547204196a0d30105708d18b6e
中汁龍 <72063585+XNORGATE@users.noreply.github.com>
2020-12-04 01:05:36 +0800

    Update server.lua

--- server.lua
+++ server.lua
@@ -2,7 +2,12 @@ ESX = nil
 
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
-RegisterServerEvent('xng_clearveh')
-AddEventHandler('xng_clearveh', function()
- TriggerClientEvent('chatMessage', -1, "[城市通知]", {238, 78, 118}, "無人車輛"..Config.clearafternoti.."秒後即將被拖吊") 
+Citizen.CreateThread(function()
+    while Config.autoclear do
+        Citizen.Wait(Config.min *60000)
+        TriggerClientEvent('chatMessage', -1, "[城市通知]", {238, 78, 118}, "無人車輛"..Config.clearafternoti.."秒後即將被拖吊") 
+        Citizen.Wait(Config.clearafternoti*1000)
+        TriggerClientEvent('xng_clearvehcl',-1)
+    end    
 end)
+
